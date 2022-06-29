135c868f4e36813ade429abfabcfa75ec83ec921
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-06-29 19:44:34 +0500

    update

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var countDate = new Date("sat, 9, july, 2022").getTime();
-var now = new Date().getTime();
-var distance = countDate - now;
+var recent = new Date().getTime();
+var distance = countDate - recent;
 
 
 var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m ";
+console.log(days + "d " + hours + "h " + minutes + "m ");
