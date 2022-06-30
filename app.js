4e5944689edbda9de946a6997fc401b442991ed5
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-06-30 19:23:56 +0500

    update

--- app.js
+++ app.js
@@ -8,4 +8,3 @@ var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
 document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m ";
-console.log(days + "d " + hours + "h " + minutes + "m ");
